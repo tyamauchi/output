369880c0de1684184b83f964d2b21697f97dea83
Takuya <yamauchi@yinteraction-design.com>
2025-02-25 10:17:21 +0900

    20250225_2

--- LinearRelaxzation/ex2.py
+++
@@ -1,9 +0,0 @@
-from pulp import *
-m = LpProblem(sense=LpMaximize) # 数理モデル
-x,y,z = [LpVariable(c,lowBound=0,upBound=1) for c in 'xyz'] # 変数
-m += lpDot([7,8,9], [x,y,z]) # 目的関数
-m += lpDot([6,7,8], [x,y,z]) <= 14 # 制約条件
-m.solve() # 求解
-print([value(v) for v in [x,y,z]]) # 出力
-#>>>
-#[1.0, 1.0, 0.125]
